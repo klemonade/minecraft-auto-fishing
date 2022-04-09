22872c8252bb4d3e4b33544e9b68fda3a2095960
slemonade <lemon.thanakrit@gmail.com>
2022-04-09 13:09:37 +0700

    multimonitor support

--- fish.py
+++ fish.py
@@ -1,6 +1,10 @@
 import pyautogui as gui
 from time import sleep
 
+from PIL import ImageGrab
+from functools import partial
+ImageGrab.grab = partial(ImageGrab.grab, all_screens=True)
+
 def fish():
     while(True):
         position = gui.locateOnScreen('detect-img.png', confidence=0.8)
